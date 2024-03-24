b8e1aa60f33af55dd33bff7f53e47157c8ee91b6
Андрей Боев <a.boev.it@gmail.com>
2024-03-24 16:00:26 +0300

    1. Введение (начало)

---
+++ 1. Intro/Intro/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    // Точка входа - функция, с которой начинается выполнение проекта.
+    public static void main(String[] args) throws IOException {
+        // Переменные
+        int num;
+        // Scanner
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        num = input.nextInt();
+
+    }
+}
