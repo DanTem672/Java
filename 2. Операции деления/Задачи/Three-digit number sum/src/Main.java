054276cd87c7f1a45044960d904d561961f30f95
Андрей Боев <a.boev.it@gmail.com>
2024-04-07 18:08:19 +0300

    Fix workspace bug

--- 2. Операции деления/Задачи/Three-digit number sum/src/Main.java
+++ 2. Операции деления/Задачи/Three-digit number sum/src/Main.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Вывести сумму цифр трёхзначного числа
-
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
-        System.out.printf("Sum = %d",(num / 100 )+ (num / 10 % 10) + (num % 10) );
+        System.out.printf("Sum = %d", (num / 100) + (num / 10 % 10) + (num % 10));
     }
 }
